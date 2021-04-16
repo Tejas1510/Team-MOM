8738b888b3dd022ee412410dbd6eea28cb40b2fc
Tejas1510 <64543913+Tejas1510@users.noreply.github.com>
2021-04-17 00:23:04 +0530

    Add files via upload

---
+++ Chrome Extension/popup.js
@@ -0,0 +1,64 @@
+extensionState = {
+    loggedIn: false,
+    email: '',
+    name: '',
+}
+
+console.log('extensionState', extensionState);
+
+let loginContainerContent = '';
+
+console.log('token from popup.js : ', localStorage.getItem('token'));
+
+updateState = () => {
+    let token = localStorage.getItem('token');
+    if (token !== null && token !== undefined) {
+        fetch('https://meetdigest.herokuapp.com/api/current_user/', {
+            headers: {
+                Authorization: `JWT ${token}`
+            }
+        })
+            .then(res => res.json())
+            .then(json => {
+                let oldContainerContent = loginContainerContent;
+                let loginContainer = document.getElementById("loginContainer");
+
+                if (json === undefined || json.email === '' || json.email === undefined) {
+                    // If the token is invalid or expired
+                    extensionState.loggedIn = false;
+                    extensionState.email = '';
+                    extensionState.name = '';
+                    loginContainerContent = ' <a href="https://meetdigest.netlify.app/login" target="_blank">Login</a> ';
+                }
+                else {
+                    // If token is correct
+                    extensionState.loggedIn = true;
+                    extensionState.email = json.email;
+                    extensionState.name = json.name;
+                    loginContainerContent = `<p> Logged In as: ${extensionState.email} (${extensionState.name}) </p>`;
+                    loginContainerContent += `<p> <a href="https://meetdigest.netlify.app/dashboard" target="_blank">View Dashboard</a> </p>`;
+                }
+                console.log("updateState called ", extensionState);
+                if (oldContainerContent != loginContainerContent) {
+                    // If the display data in html has changed, make update
+                    loginContainer
+                    while (loginContainer.firstChild) {
+                        // Remove any nodes inside loginContainer if already present
+                        loginContainer.removeChild(loginContainer.firstChild);
+                    }
+                    loginContainer.innerHTML = loginContainerContent;
+                }
+            });
+    }
+    else {
+        console.log("popup.js: token not found in localstorage");
+        let loginContainer = document.getElementById("loginContainer");
+        extensionState.loggedIn = false;
+        extensionState.email = '';
+        extensionState.name = '';
+        loginContainerContent = ' <a href="https://meetdigest.netlify.app/login" target="_blank">Login</a> ';
+        loginContainer.innerHTML = loginContainerContent;
+    }
+}
+
+updateState();
